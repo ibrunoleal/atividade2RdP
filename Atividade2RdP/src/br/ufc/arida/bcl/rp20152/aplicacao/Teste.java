ea5372e898683939dda271bff23984283ad7bbd5
brunoleal <ibrunoleal@gmail.com>
2015-09-28 15:16:20 -0300

    adicionado calculos de mean e covariancia

--- Atividade2RdP/src/br/ufc/arida/bcl/rp20152/aplicacao/Teste.java
+++ Atividade2RdP/src/br/ufc/arida/bcl/rp20152/aplicacao/Teste.java
@@ -2,7 +2,7 @@ package br.ufc.arida.bcl.rp20152.aplicacao;
 
 import br.ufc.arida.bcl.rp20152.arquivos.FileHandler;
 import br.ufc.arida.bcl.rp20152.entidades.Matriz;
-import br.ufc.arida.bcl.rp20152.entidades.Vetor;
+
 
 public class Teste {
 
@@ -12,14 +12,10 @@ public class Teste {
 		Matriz matriz = new Matriz(fileHandler.getMatrizDeEntrada().getMatriz(),
 									fileHandler.getNumeroDeLinhas(), fileHandler.getNumeroDeColunas());
 		
-		Vetor x1 = new Vetor(matriz.getColuna(0).getVetor());
-		System.out.println(x1);
-		
-		Vetor x2 = new Vetor(matriz.getColuna(1).getVetor());
-		System.out.println(x2);
-		
-		Vetor x3 = new Vetor(matriz.getColuna(2).getVetor());
-		System.out.println(x3);		
+		//imprimir matriz
+		for (int i = 0; i < matriz.getNumeroDeLinhas(); i++) {
+			System.out.println(matriz.getLinha(i));
+		}
 	}
 
 }
--- Atividade2RdP/src/br/ufc/arida/bcl/rp20152/entidades/Vetor.java
+++ Atividade2RdP/src/br/ufc/arida/bcl/rp20152/entidades/Vetor.java
@@ -20,6 +20,44 @@ public class Vetor {
 		this.vetor = vetor;
 	}
 	
+	public double getMean() {
+		double soma = 0;
+		for (int i = 0; i < vetor.length; i++) {
+			soma += vetor[i];
+		}
+		double mean = soma/vetor.length;
+		return mean;
+	}
+	
+	public double getVariance() {
+		double mean = getMean();
+		int n = vetor.length;
+		double somatorio = 0;
+		
+		for (int i = 0; i < n; i++) {
+			somatorio += Math.pow((vetor[i] - mean),2);
+		}
+		
+		double variance = somatorio / n;
+		return variance;
+	}
+	
+	public double getCovariance(Vetor y) {
+		double eX = this.getMean();
+		double eY = y.getMean();
+		double eXY = 0;
+		int n = vetor.length;
+		
+		double somaAux = 0;
+		for (int i = 0; i < vetor.length; i++) {
+			somaAux = vetor[i] * y.getVetor()[i];
+		}
+		eXY = somaAux / n;
+		
+		double covariance = eXY - (eX * eY);
+		return covariance;
+	}
+	
 	public String toString() {
 		String resultado = "[";
 		for (int i = 0; i < (vetor.length - 1); i++) {
